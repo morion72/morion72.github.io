9f4c22c47535d92845ab2d92e5132029ccddb070
morion72 <maxsova777@gmail.com>
2025-07-29 20:46:21 +0700

    Fix: Force track 'out' directory

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   distDir: 'docs',
+  assetPrefix: './', // Ключевой момент: относительные пути!
   typescript: {
     ignoreBuildErrors: true,
   },
